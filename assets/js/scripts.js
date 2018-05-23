e970f26bff3e8b641ada260a4a4e07f4d70c14c2
Bradley Hawkins <>
2018-05-22 22:31:57 -0400

    New portfolio launch woo!

---
+++ assets/js/scripts.js
@@ -0,0 +1,52 @@
+// Kids Discover
+jQuery(document).on("mouseenter", ".kidsdiscover", function(){
+jQuery("body").css("background-image", "linear-gradient(#fffbe3, #fffbe3)");
+});
+
+jQuery(document).on("mouseleave", ".kidsdiscover", function(){
+jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
+});
+
+
+// Trytn
+jQuery(document).on("mouseenter", ".trytn", function(){
+jQuery("body").css("background-image", "linear-gradient(#d2fff6, #d2fff6)");
+});
+
+jQuery(document).on("mouseleave", ".trytn", function(){
+jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
+});
+
+
+
+// Hatchback
+jQuery(document).on("mouseenter", ".hatchback", function(){
+jQuery("body").css("background-image", "linear-gradient(#fffbe3, #fffbe3)");
+});
+
+jQuery(document).on("mouseleave", ".hatchback", function(){
+jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
+});
+
+
+
+// Milkmoney
+jQuery(document).on("mouseenter", ".milkmoney", function(){
+jQuery("body").css("background-image", "linear-gradient(#fef5fb, #fef5fb)"); 
+});
+
+jQuery(document).on("mouseleave", ".milkmoney", function(){
+jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
+});
+
+// Andculture
+jQuery(document).on("mouseenter", ".andculture", function(){
+jQuery("body").css("background-image", "linear-gradient(#fdf1e5, #fdf1e5)"); 
+});
+
+jQuery(document).on("mouseleave", ".andculture", function(){
+jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
+});
+
+
+
