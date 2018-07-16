18d0b295fac2345a1584baebc485b9f192037eec
Bradley Hawkins <>
2018-07-16 16:41:14 -0400

    Adjustments to background color styling.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,50 @@
 // Kids Discover
-jQuery(document).on("mouseenter", ".kidsdiscover", function(){
+jQuery(document).on("mouseenter", "div.kidsdiscover", function(){
 jQuery("body").css("background-image", "linear-gradient(#fffbe3, #fffbe3)");
 });
 
-jQuery(document).on("mouseleave", ".kidsdiscover", function(){
+jQuery(document).on("mouseleave", "div.kidsdiscover", function(){
 jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
 });
 
 
 // Trytn
-jQuery(document).on("mouseenter", ".trytn", function(){
+jQuery(document).on("mouseenter", "div.trytn", function(){
 jQuery("body").css("background-image", "linear-gradient(#d2fff6, #d2fff6)");
 });
 
-jQuery(document).on("mouseleave", ".trytn", function(){
+jQuery(document).on("mouseleave", "div.trytn", function(){
 jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
 });
 
 
 
 // Hatchback
-jQuery(document).on("mouseenter", ".hatchback", function(){
+jQuery(document).on("mouseenter", "div.hatchback", function(){
 jQuery("body").css("background-image", "linear-gradient(#fffbe3, #fffbe3)");
 });
 
-jQuery(document).on("mouseleave", ".hatchback", function(){
+jQuery(document).on("mouseleave", "div.hatchback", function(){
 jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
 });
 
 
 
 // Milkmoney
-jQuery(document).on("mouseenter", ".milkmoney", function(){
+jQuery(document).on("mouseenter", "div.milkmoney", function(){
 jQuery("body").css("background-image", "linear-gradient(#fef5fb, #fef5fb)"); 
 });
 
-jQuery(document).on("mouseleave", ".milkmoney", function(){
+jQuery(document).on("mouseleave", "div.milkmoney", function(){
 jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
 });
 
 // Andculture
-jQuery(document).on("mouseenter", ".andculture", function(){
+jQuery(document).on("mouseenter", "div.andculture", function(){
 jQuery("body").css("background-image", "linear-gradient(#fdf1e5, #fdf1e5)"); 
 });
 
-jQuery(document).on("mouseleave", ".andculture", function(){
+jQuery(document).on("mouseleave", "div.andculture", function(){
 jQuery("body").css("background-image", "linear-gradient(#f7f7f7, #f7f7f7)");
 });
 
